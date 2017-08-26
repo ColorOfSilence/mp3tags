2af0e49e58bd1f3a8b832e68a8e830feb2326379
jdonosa <juandonosa@hotmail.com>
2017-08-26 16:10:26 -0700

    house keeping, adding comments to sections

--- src/mp3tags/mp3tags.java
+++ src/mp3tags/mp3tags.java
@@ -1,7 +1,33 @@
 package mp3tags;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
 public class mp3tags {
 	public static void main(String[] args) {
+		mp3tags();
+	}
+	
+	public static void mp3tags() {
+		/*
+		 * Setup the variables what I will need to be using first
+		 */
+		String artist = "", album = "";
+		int totTracks = 0;
+		int[] tracks;
+		String[] trackTitles;
 		
+		//This queries the user for how many tracks the album has so that it can initialize
+		
+		
+		//This is to choose the current .txt file with the initial information to be converted to ffmpeg mp3tags
+		File file = null;
+		JFileChooser chooser = new JFileChooser();
+		int returnVal = chooser.showOpenDialog(null);
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+			String fullPath = chooser.getSelectedFile().getAbsolutePath();
+			file = new File(fullPath);
+		}
 	}
 }
